dbf7a62c2d2f55e8427bc79d54ecf4c5b21387e8
Rastislav Kuka <rastislav.kuka@seznam.cz>
2023-10-20 12:21:02 +0200

    Input format checking implemented

--- src/main/java/com/example/gpsparser/services/ParserServiceImpl.java
+++ src/main/java/com/example/gpsparser/services/ParserServiceImpl.java
@@ -4,23 +4,22 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ParserServiceImpl implements ParserService {
+
     @Override
     public boolean isDecimalDegrees(String input) {
-        char[] chars = input.toCharArray();
-        if (chars[2] == '.') {
-            return true;
-        }
-        return false;
+        return input.toCharArray()[2] == '.';
     }
 
     @Override
     public boolean isDegreesWithDecimalMinutes(String input) {
-        return false;
+        return input.toCharArray()[2] == '°' &&
+                input.toCharArray()[5] == '.';
     }
 
     @Override
     public boolean isDegreesMinutesSeconds(String input) {
-        return false;
+        return input.toCharArray()[2] == '°' &&
+                input.toCharArray()[5] == '\'';
     }
 
     @Override
