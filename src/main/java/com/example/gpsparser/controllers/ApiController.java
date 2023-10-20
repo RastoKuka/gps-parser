b2e2c8f2383517191b877903ff3681d98e6e471d
Rastislav Kuka <rastislav.kuka@seznam.cz>
2023-10-20 12:49:52 +0200

    ÅAPI controller implemented

--- src/main/java/com/example/gpsparser/controllers/ApiController.java
+++ src/main/java/com/example/gpsparser/controllers/ApiController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.management.Notification;
+
 @RestController
 @RequestMapping("/api")
 public class ApiController {
@@ -21,10 +23,33 @@ public class ApiController {
     public ResponseEntity<?> parseCoordinates(@RequestBody InputDto inputDto) {
         if (inputDto.getInputLatitude().isEmpty() && inputDto.getInputLongitude().isEmpty()) {
             return ResponseEntity.badRequest().body("Provide at least one coordinate!");
-        } else if (parserService.isDecimalDegrees(inputDto.getInputLatitude()) &&
-                parserService.isDecimalDegrees(inputDto.getInputLongitude())) {
-            return ResponseEntity.status(201).body("Longitude" + parserService.parseFromDecimalDegrees(inputDto.getInputLongitude()) +
-                    "Latitude" + parserService.parseFromDecimalDegrees(inputDto.getInputLatitude()));
         }
+            String inputLongitude = inputDto.getInputLongitude();
+            String inputLatitude = inputDto.getInputLatitude();
+            String outputLongitude = "";
+            String outputLatitude = "";
+
+            if (parserService.isDecimalDegrees(inputLatitude)) {
+                outputLatitude = String.valueOf(parserService.parseFromDecimalDegrees(inputLatitude));
+            } else if (parserService.isDegreesWithDecimalMinutes(inputLatitude)) {
+                outputLatitude = String.valueOf(parserService.parseFromDegreesWithDecimalMinutes(inputLatitude));
+            } else {
+                outputLatitude = String.valueOf(parserService.parseFromDegreesMinutesSeconds(inputLatitude));
+            }
+
+            if (parserService.isDecimalDegrees(inputLongitude)) {
+                outputLongitude = String.valueOf(parserService
+                        .parseFromDecimalDegrees(inputLongitude));
+            } else if (parserService.isDegreesWithDecimalMinutes(inputLongitude)) {
+                outputLongitude = String.valueOf(parserService
+                        .parseFromDegreesWithDecimalMinutes(inputLongitude));
+            } else {
+                outputLongitude = String.valueOf(parserService
+                        .parseFromDegreesMinutesSeconds(inputLongitude));
+            }
+
+            return ResponseEntity.ok().body("Longitude: " + outputLongitude +
+                    "Latitude: " + outputLatitude);
+        }
+       // return ResponseEntity.badRequest().body("Invalid coordinates");
     }
-}
